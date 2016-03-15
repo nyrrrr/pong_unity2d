c77151706508682eac063ca47af2c3f8da088665
nyrrrr <xnamespooferx@gmail.com>
2016-03-15 02:26:30 +0100

    fixed position with changing resolution

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    Transform go;
+    Vector2 paddlePlayerPos;
+
+    void Awake()
+    {
+        go = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _PositionPaddleOnLeftSideOfScreen();
+    }
+
+    private void _PositionPaddleOnLeftSideOfScreen()
+    {
+        paddlePlayerPos = new Vector2(32, 0);
+        paddlePlayerPos.x = (Camera.main.ScreenToWorldPoint(paddlePlayerPos)).x;
+        transform.position = paddlePlayerPos;
+    }
+}
