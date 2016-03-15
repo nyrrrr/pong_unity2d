bb5391d44648a7e0e9ece6029d10b2549ffa73d3
nyrrrr <xnamespooferx@gmail.com>
2016-03-15 20:15:40 +0100

    fix on start position and second paddle added

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,18 +9,25 @@ public class PlayerControl : MonoBehaviour
     float fVertical = 0;
     float fSpeed = 13f;
     Rigidbody2D rigGo;
+    public bool isPlayerA = true;
 
+    // init
     void Awake()
     {
         go = this.transform;
         rigGo = this.GetComponent<Rigidbody2D>();
     }
 
+    // start setup
+    void Start() {
+        go.position = new Vector2(go.position.x, 0);
+    }
+
     // Update is called once per frame
     void Update()
     {
         _PositionPaddleOnLeftSideOfScreen();
-        _MoveOnInput();
+        if (isPlayerA) _MoveOnInput();
     }
 
     private void _MoveOnInput()
@@ -31,14 +38,16 @@ public class PlayerControl : MonoBehaviour
             rigGo.velocity = Vector2.up * fVertical * fSpeed;
         }
         else rigGo.velocity = Vector2.zero;
-            //go.position = new Vector2(go.position.x, go.position.y + (fVertical * fSpeed));
     }
 
     private void _PositionPaddleOnLeftSideOfScreen()
     {
-        vPaddlePlayerPos = new Vector2(32, 0);
+        if (isPlayerA)
+            vPaddlePlayerPos = new Vector2(32, 0);
+        else
+            vPaddlePlayerPos = new Vector2(Screen.width - 32, 0);
         vPaddlePlayerPos.x = (Camera.main.ScreenToWorldPoint(vPaddlePlayerPos)).x;
-        vPaddlePlayerPos.y = go.position.y;
+        vPaddlePlayerPos.y = 0;
         go.position = vPaddlePlayerPos;
     }
 }
