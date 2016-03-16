de8f8e4d99333f797ea472c6245cc7e69265f74c
nyrrrr <xnamespooferx@gmail.com>
2016-03-16 02:42:15 +0100

    apparently i did something… (sound)

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,21 +3,25 @@ using System.Collections;
 
 public class Ball : MonoBehaviour
 {
+    public AudioClip aHit, aScore;
 
     Rigidbody2D rigGo;
     Transform go;
     float fSpeed = 8f, fCollisionValue = 0f, fMaxSpeed = 14f;
+    AudioSource audio;
+
     // init
     void Awake()
     {
         go = this.transform;
         rigGo = this.GetComponent<Rigidbody2D>();
+        audio = this.GetComponent<AudioSource>();
+        audio.clip = aHit;
     }
 
     // start setup
     void Start()
     {
-
         rigGo.velocity = new Vector2(Random.Range(-0.5f, 0.5f), Random.Range(-0.25f, 0.25f)).normalized * fSpeed;
     }
 
@@ -30,10 +34,30 @@ public class Ball : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.tag == "Player" || col.gameObject.tag == "Player or AI") { 
-        fCollisionValue = _GetCollisionAngle(go.position.y, col.transform.position.y, col.collider.bounds.size.y);
-        rigGo.velocity = new Vector2((go.position.x > col.transform.position.x ? 1 : -1), fCollisionValue).normalized * fSpeed;
+        _PlaySound(col);
+        _Bounce(col);
+    }
+
+    private void _Bounce(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag == "Player or AI")
+        {
+            fCollisionValue = _GetCollisionAngle(go.position.y, col.transform.position.y, col.collider.bounds.size.y);
+            rigGo.velocity = new Vector2((go.position.x > col.transform.position.x ? 1 : -1), fCollisionValue).normalized * fSpeed;
         }
+    }
+
+    private void _PlaySound(Collision2D col)
+    {
+        if (col.gameObject.tag != "Finish")
+        {
+            audio.clip = aHit;
+        }
+        else
+        {
+            audio.clip = aScore;
+        }
+        audio.Play();
     }
 
     // not calculating a real "angle"
