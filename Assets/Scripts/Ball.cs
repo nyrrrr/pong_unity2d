9801a5d4b37b28da38cf20d6f0f4347b2be8abb3
nyrrrr <xnamespooferx@gmail.com>
2016-03-16 01:55:43 +0100

    ball physic now depends on where the ball is hit

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,8 +6,7 @@ public class Ball : MonoBehaviour
 
     Rigidbody2D rigGo;
     Transform go;
-    float fSpeed = 8f;
-
+    float fSpeed = 8f, fCollisionValue = 0f;
     // init
     void Awake()
     {
@@ -18,7 +17,7 @@ public class Ball : MonoBehaviour
     // start setup
     void Start()
     {
-        
+
         rigGo.velocity = new Vector2(Random.Range(-0.5f, 0.5f), Random.Range(-0.25f, 0.25f)).normalized * fSpeed;
         //transform.Translate(Vector3(randomX, randomY, 0) * moveSpeed * Time.deltaTime);
     }
@@ -29,7 +28,16 @@ public class Ball : MonoBehaviour
         if (rigGo.velocity.magnitude < fSpeed) rigGo.velocity = rigGo.velocity.normalized * fSpeed;
     }
 
-    void OnCollisionEnter2D(Collision2D col) {
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag == "Player or AI") { 
+        fCollisionValue = _GetCollisionAngle(go.position.y, col.transform.position.y, col.collider.bounds.size.y);
+        rigGo.velocity = new Vector2((go.position.x > col.transform.position.x ? 1 : -1), fCollisionValue).normalized * fSpeed;
+        }
+    }
 
+    private float _GetCollisionAngle(float fBallY, float fPaddleY, float fPaddleSize)
+    {
+        return (fPaddleY - fBallY) / fPaddleSize;
     }
 }
